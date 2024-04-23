0f45d75648c1834364f71f528bd8f1897121c5da
小鍋 <l09250701@gmail.com>
2024-04-23 22:17:46 +0800

    引用view ui + header完成 - 漢堡選單詳細內容

--- src/components/Mainheader.vue
+++ src/components/Mainheader.vue
@@ -1,38 +1,48 @@
 <template>
-    <div id="header">
-        <div class="headerWrapper">
-            <div class="logo">
-                <img src="../assets/images/foodhub-logo.png" alt="logo">
-            </div>
-            <div class="localSearchContent">
-              <div class="local">
-                <i class="fa-solid fa-location-dot"></i>
-                <span>New York</span>
-              </div>
-              <div class="gps">
-                <button><img src="../assets/images/gpsIcon.svg" alt="gps"></button>
-              </div>
-              <input type="text" placeholder="Search for restaurant ">
-              <button>
-                <i class="fa-solid fa-magnifying-glass"></i>
-              </button>
-
-            </div>
-
-            <div class="account">
-                <img src="../assets/images/accountIcon.svg" alt="account">
-                <span>ACCOUNT</span>
-            </div>
-            <div class="cart">
-              <i class="fa-solid fa-cart-shopping"></i>
-              <span>(8)</span>
-            </div>
-            <!-- 漢堡 -->
-            <button>
-              <i class="fa-solid fa-bars"></i>
-            </button>
-
+  <div id="header">
+    <div class="headerWrapper container">
+      <div class="logo">
+          <img src="../assets/images/foodhub-logo.png" alt="logo">
+      </div>
+      <div class="localSearchContent">
+        <div class="local">
+          <i class="fa-solid fa-location-dot"></i>
+          <span class="localName">New York</span>
         </div>
+        <div class="gps">
+          <button><img src="../assets/images/gpsIcon.svg" alt="gps"></button>
+        </div>
+        <div class="searchBar">
+          <input type="text" placeholder="Search for restaurant ">
+          <button>
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+
+      </div>
+
+      <div class="account">
+          <img src="../assets/images/accountIcon.svg" alt="account">
+          <span class="accountName">ACCOUNT</span>
+      </div>
+      <div class="cart">
+        <i class="fa-solid fa-cart-shopping"></i>
+        <span>(8)</span>
+      </div>
+      <!-- 漢堡 -->
+      <button class="menu" @click="Openmenu()">
+        <i class="fa-solid fa-bars"></i>
+      </button>
+      <transition name="slide-left">
+        <aside class="hamWrapper openMenu" v-if="menuOpen">
+          <div class="hamTitle">
+            <span>Navbar</span>
+            <i class="fa-solid fa-xmark  closeIcon"  @click="Openmenu()"></i>
+          </div>
+
+        </aside>
+      </transition>
+
 
 
 
@@ -40,23 +50,36 @@
 
 
     </div>
+
+
+
+
+
+
+  </div>
 </template>
 
 <script>
+import { RouterLink } from "vue-router";
+
 export default {
+    data() {
+        return {
+            menuOpen: false,
+        }
+    },
+    methods: {
+        Openmenu(){
+            this.menuOpen=!this.menuOpen
+            
+        }
+    },
+    mounted() {
+        // 監聽路由切換
+    this.$router.afterEach(() => {
+      this.menuOpen = false; // 關閉漢堡選單
+    });
 
-  data() {
-    return {
-      
-    };
-  },
-
-  mounted() {
-    
-  },
-
-  methods: {
-    
-  },
-};
+    },
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -11,9 +11,14 @@ import router from './router'
 //引入fontawesome
 import '@fortawesome/fontawesome-free/css/all.css'
 
+//引入view ui
+import ViewUIPlus from 'view-ui-plus'
+import "view-ui-plus/dist/styles/viewuiplus.css";
+
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ViewUIPlus)
 app.mount('#app')
