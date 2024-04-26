b9d66c3d43a43c502b7965576fcb20657db97d30
小鍋 <l09250701@gmail.com>
2024-04-27 02:12:57 +0800

    foodmenu init

---
+++ src/views/FoodMenu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="foodMenu">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+        
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
